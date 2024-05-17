971e18d78c1664a4b3ba27ff7437e7decb4cfb6f
LiyandPeter <168171761+LiyandPeter@users.noreply.github.com>
2024-05-17 15:30:49 +0800

    Add files via upload

---
+++ Analog/Analog.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your m
+  //ain code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  if (sensorValue >= 500)sensorValue = 500;
+  else if(sensorValue <= 100)sensorValue = 100;
+  int val = map(sensorValue,100,500,0,255);
+  analogWrite(9,val);
+	//val == 500 ;
+  delay(10);
+}
